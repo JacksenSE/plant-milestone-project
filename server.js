b21a916f9d7045a94969a65e9c79e7758da3ce25
JacksenSE <109568831+JacksenSE@users.noreply.github.com>
2023-02-20 14:04:24 -0800

    wa

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const path = require('path');
 const PORT = process.env.PORT
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, '/build')));
 app.get('/', (req, res) => {
-  res.send('Hello, world!')
+    res.sendFile(path.join(__dirname, '/build', 'index.html'));
 })
+
 
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`))
 
--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import React from 'react';
 import CarouselPage from './Components/Carousel/Carousel';
 import Carousel from "./Components/Carousel/Carousel";
+import NavbarComp from './Components/Navbar/NavbarComp';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <><div className="App">
       <CarouselPage />
-      <Carousel/>
-    </div>
+      <NavbarComp />
+      <h1 style={styles}>House-Plant Watering Log</h1>
+      <PlantProfiles />
+    </div><BrowserRouter>
+        <nav>
+          <Link to="/"></Link>
+          <Link to="/create"></Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<PlantProfiles/>} />
+          <Route path="/create" element={<NavbarComp />} />
+          <Route path="/:id" element={<Carousel />} />
+        </Routes>
+      </BrowserRouter></>
+      
+     
   );
+  
 }
-
+const styles = {
+  color: 'green',
+  backgroundColor: 'salmon',
+  fontSize: '40px',
+  margin: '20px'
+}
 export default App;
